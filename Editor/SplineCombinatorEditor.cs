39b55cb013866d9ba04c758e831753bd2ccc6e09
Matthew Brown <60948236+Mushakushi@users.noreply.github.com>
2024-02-27 17:17:07 -0500

    Add files via upload
    actually added meta files properly

---
+++ Editor/SplineCombinatorEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SplineCombinator))]
+public class SplineCombinatorEditor : Editor
+{
+    private SplineCombinator script;
+
+    private void OnEnable()
+    {
+        script = (SplineCombinator)target;
+    }
+
+    public override void OnInspectorGUI() 
+    {
+        serializedObject.Update();
+        DrawDefaultInspector();
+        if (GUILayout.Button("Generate Combined Spline"))
+        {
+            script.GenerateCombinedSpline();
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
